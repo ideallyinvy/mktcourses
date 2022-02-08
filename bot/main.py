04625a5f6851cacc91bb896f8a3cd1c168fd5cd9
itzinvy <52226069+itzinvy@users.noreply.github.com>
2022-02-08 18:00:05 -0330

    Added wonderwords import

--- bot/main.py
+++ bot/main.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from random import *
 from datetime import datetime
 import pytz
+from wonderwords import RandomWord
 intents = discord.Intents.default()
 intents.members = True
 
